84d5d44e1204684a9e23819cdbab1186e51bec68
cenyg <test@test.test>
2022-01-16 19:56:05 +0300

    test bool

--- Tests/MatrixTest.cs
+++ Tests/MatrixTest.cs
@@ -52,8 +52,6 @@ namespace Tests
             var m1 = rw.ReadFile($"{_testDir}\\matrix_double1.txt");
             var m2 = rw.ReadFile($"{_testDir}\\matrix_double2.txt");
             
-            // convert to MyInt
-
             var mm1 = new Matrix<double>(m1);
             var mm2 = new Matrix<double>(m2);
             
@@ -79,6 +77,42 @@ namespace Tests
             var resFileContent = File.ReadAllLines(resFileDir);
             Assert.AreEqual("43.5;75.75", resFileContent[0]);
             Assert.AreEqual("2;0.5", resFileContent[1]);
+            
+            File.Delete(resFileDir);
+        }
+        
+        [Test]
+        public void BoolMatrix()
+        {
+            var rw = new ReaderWriter<bool>();
+            var m1 = rw.ReadFile($"{_testDir}\\matrix_bool1.txt");
+            var m2 = rw.ReadFile($"{_testDir}\\matrix_bool2.txt");
+            
+            var mm1 = new Matrix<bool>(m1);
+            var mm2 = new Matrix<bool>(m2);
+            
+            Console.WriteLine("First matrix:");
+            Console.WriteLine(mm1);
+            Console.WriteLine("Second matrix:");
+            Console.WriteLine(mm2);
+
+            var mm3 = mm1.Mul(mm2);
+
+            Console.WriteLine("Result matrix:");
+            Console.WriteLine(mm3);
+            
+
+            var expected = new[] {new[]{true, true}, new[]{true, true}};
+            Assert.AreEqual(expected, mm3.Data);
+
+            var resFileDir = $"{_testDir}\\resMatrix_double.txt";
+            rw.WriteFile(mm3.Data, resFileDir);
+
+            Assert.True(File.Exists(resFileDir));
+
+            var resFileContent = File.ReadAllLines(resFileDir);
+            Assert.AreEqual("True;True", resFileContent[0]);
+            Assert.AreEqual("True;True", resFileContent[1]);
             
             File.Delete(resFileDir);
         }
